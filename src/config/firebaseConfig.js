7c2caf6abdebb801ec739948e348010eca138015
Raman-Maurya <raman.2005kumar@gmail.com>
2025-05-01 16:23:43 +0530

    fixed deployment

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBot-zD1FKjyvH8e2oijsump6RpIXgtoLM",
+//     authDomain: "crypto-tracker-92dbb.firebaseapp.com",
+//     projectId: "crypto-tracker-92dbb",
+//     storageBucket: "crypto-tracker-92dbb.appspot.com",
+//     messagingSenderId: "96964123585",
+//     appId: "1:96964123585:web:4d89b317c2ed04056e2d40"
+//   };
+  const firebaseConfig = {
+    authDomain: "https://rmn-crypto-tracker.netlify.app/",
+    apiKey: "AIzaSyAz4ZGQ2nz4skRzJd-0_uMXOu8_WzeboAY",
+    authDomain: "c-tracker-27fe5.firebaseapp.com",
+    projectId: "c-tracker-27fe5",
+    storageBucket: "c-tracker-27fe5.firebasestorage.app",
+    messagingSenderId: "1070112523613",
+    appId: "1:1070112523613:web:bb7e2cf6359c5d751554b4"
+  };
+  export default firebaseConfig;
